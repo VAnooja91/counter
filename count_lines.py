46e22c4db7b1f4add1dd2a49007999583a6d8ea3
Anooja <anoojavardhineni@gmail.com>
2025-10-07 17:17:33 +0530

    added a function for further utilisation of code

--- count_lines.py
+++ count_lines.py
@@ -1,8 +1,10 @@
-"""Counts the number of lines in a file"""
-counter = 0
-with open("story.txt", "r") as FH:
-    for line in FH:
-        counter += 1
-        
-print("total count of lines:", counter)
-    
+def count_lines():
+    """Counts the number of lines in a file"""
+    counter = 0
+    with open("story.txt", "r") as FH:
+        for line in FH:
+            counter += 1
+    return counter
+
+
+print("total count of lines:", count_lines())
