8c35181ce5324e9f4ffbf22ac394f804a5522004
Anooja <anoojavardhineni@gmail.com>
2025-10-08 16:26:31 +0530

    added the skip_empty parameter to skip the empty lines

--- count_lines.py
+++ count_lines.py
@@ -1,10 +1,16 @@
-def count_lines(f_name):
+def count_lines(f_name, skip_empty=False):
     """Counts the number of lines in a file"""
     counter = 0
     with open(f_name, "r") as FH:
         for line in FH:
-            counter += 1
+            if line != "\n":
+                counter += 1
+            elif skip_empty == False and line == "\n":
+                counter += 1
+            #print(counter ,repr(line))
     return counter
 
 
+
 print("total count of lines:", count_lines("story.txt"))
+print("total count of lines:", count_lines("story.txt", skip_empty=True))
