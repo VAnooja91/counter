e017cd4d425ae553f9c1165b55d1eaefe6f720ca
Anooja <anoojavardhineni@gmail.com>
2025-10-08 15:45:42 +0530

    filename as a parameter in the function

--- count_lines.py
+++ count_lines.py
@@ -1,10 +1,10 @@
-def count_lines():
+def count_lines(f_name):
     """Counts the number of lines in a file"""
     counter = 0
-    with open("story.txt", "r") as FH:
+    with open(f_name, "r") as FH:
         for line in FH:
             counter += 1
     return counter
 
 
-print("total count of lines:", count_lines())
+print("total count of lines:", count_lines("story.txt"))
