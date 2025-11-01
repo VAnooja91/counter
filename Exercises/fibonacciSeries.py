5f4165c230e697eb4d505ffd73e068e85e17f10f
Anooja <anoojavardhineni@gmail.com>
2025-11-01 12:36:43 +0530

    using  '_' for unused varible

--- Exercises/fibonacciSeries.py
+++ Exercises/fibonacciSeries.py
@@ -5,7 +5,7 @@ def fibonacciSeries(num):
     print(fibo1)
     print(fibo2)
 
-    for i in range(num - 2):
+    for _ in range(num - 2):
         temp = fibo1 + fibo2
         fibo1 = fibo2
         fibo2 = temp
